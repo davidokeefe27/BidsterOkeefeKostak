581ae23263b5b635ae06f3099fcf476b5480a4b1
David O'Keefe <david.mathew.okeefe@gmail.com>
2018-10-01 19:04:16 -0400

    fisnished prep work moving to database change html to ejs

--- config/database.js
+++ config/database.js
@@ -1,1 +1,6 @@
 //database.js
+module.exports = {
+
+    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+
+app.get('/about', function (req, res) {
+  res.render('BidsterAboutPage')
+})
+
+app.get('/auction', function (req, res) {
+ res.render('BidsterAuctionPage')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
