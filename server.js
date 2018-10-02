fbec536b97a916f68b253b561367992cc1f22336
David O'Keefe <david.mathew.okeefe@gmail.com>
2018-10-02 15:07:02 -0400

    install more npm directories

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
+
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
